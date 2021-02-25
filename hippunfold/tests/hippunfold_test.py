a2d6eb57675e5c979ca87e0c019270d4ee7255d6
akhanf <alik@robarts.ca>
2021-02-24 22:01:02 -0500

    added the other dry run tests

--- hippunfold/tests/hippunfold_test.py
+++ hippunfold/tests/hippunfold_test.py
@@ -2,11 +2,16 @@ import os
 import tempfile
 
 
-def test_single_t1w(script_runner):
+def test_dry_runs(script_runner):
     os.environ['HIPPUNFOLD_CACHE_DIR'] = os.path.join(os.getcwd(),'test_data/fake_models')
     with tempfile.TemporaryDirectory() as output_dir:
-        print('created temporary directory', output_dir)
         ret = script_runner.run('hippunfold', 'test_data/bids_singleT2w',output_dir,'participant','-np')
         assert ret.success
-#    assert ret.stdout == '3.2.1\n'
-#    assert ret.stderr == ''
+
+    with tempfile.TemporaryDirectory() as output_dir:
+        ret = script_runner.run('hippunfold', 'test_data/bids_multiT2w',output_dir,'participant','-np')
+        assert ret.success
+
+    with tempfile.TemporaryDirectory() as output_dir:
+        ret = script_runner.run('hippunfold', 'test_data/bids_T1w',output_dir,'participant','-np','--modality','T1w')
+        assert ret.success
