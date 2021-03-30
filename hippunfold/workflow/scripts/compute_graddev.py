1e664891424dd12534c1610bdff1e365d7e7d6e7
Bradley-Karat <bkarat@uwo.ca>
2021-03-30 10:52:52 -0400

    Added generic python rule and script to compute the graddev from the jacobian

---
+++ hippunfold/workflow/scripts/compute_graddev.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import nibabel as nib
+from scipy.linalg import polar
+
+in_jac = snakemake.input[0] 
+out_graddev = snakemake.output[0]
+
+jac_nib = nib.load(in_jac)
+jac_data = jac_nib.get_fdata()
+
+#shape of Jacobian is (256, 128, 16, 9)
+
+#Reshape to 3x3 for decomposition
+graddev = jac_data.reshape(-1,3,3,order='F')
+
+temp_graddev=np.copy(graddev)
+graddev[:]=np.NaN
+
+#Polar decomposition of jacobian where we only take rotation
+for g in range(0,graddev.shape[0]):
+    m=temp_graddev[g,:,:]
+    x = ~np.isnan(m)
+    if x.all(): 
+        rot, deform = polar(m)
+        graddev[g,:,:]= rot
+
+grad_hold = graddev - np.identity(3)
+
+#reshape back to 256,128,16,9 for saving
+grad_dev = grad_hold.reshape(jac_data.shape,order='F')
+
+grad_nib= nib.Nifti1Image(grad_dev,jac_nib.affine)
+nib.save(grad_nib,out_graddev)
+
