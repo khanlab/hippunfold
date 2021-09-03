5b519a73513b29d4a8a1e327de7cb1f8e4cc6fde
Ali Khan <alik@robarts.ca>
2021-09-03 14:34:00 -0400

    fixes to get running and streamlining code
    - different logging
    - uses tmpdir for nighres outputs so untracked files are not created
    - uses returned dict to get output filenames instead of find/replace

--- hippunfold/workflow/scripts/isovolume_coords.py
+++ hippunfold/workflow/scripts/isovolume_coords.py
@@ -2,22 +2,29 @@ import os
 import nighres
 from shutil import copyfile
 
-logfile = open(snakemake.log[0], 'w')
-print(f'start', file=logfile, flush=True)
 
-outfile = snakemake.output.coords
-path,filename = os.path.split(outfile)
+with open(snakemake.log[0], "w") as f:
+    sys.stderr = sys.stdout = f
 
-## convert binarized edges to levelset surfaces
-nighres.surface.probability_to_levelset(snakemake.input.innerbin, save_data=True, output_dir=path)
-nighres.surface.probability_to_levelset(snakemake.input.outerbin, save_data=True, output_dir=path)
-print(f'binarized files to levelsets complete', file=logfile, flush=True)
+    print('start')
 
-## get isovolume solution
-inbin = snakemake.input.innerbin
-outbin = snakemake.input.outerbin
-nighres.laminar.volumetric_layering(inbin.replace('.nii.gz','p2l-surf.nii.gz'), outbin.replace('.nii.gz','p2l-surf.nii.gz'),, save_data=True, output_dir=path)
-print(f'levelset to ', file=logfile, flush=True)
+    nighres_args = {'save_data': True, 'output_dir': tmpdir, 'overwrite': True}
 
-## copy to output (volumetric_layering produces multiple files so we need to specify the right one)
-copyfile(path + 'innersurf_bin_p2l-surf_layering-depth.nii.gz', snakemake.output.coords)
+    tmpdir = snakemake.resources.tmpdir
+
+    ## convert binarized edges to levelset surfaces
+    levelset_inner = nighres.surface.probability_to_levelset(snakemake.input.innerbin, **nighres_args)
+    levelset_outer = nighres.surface.probability_to_levelset(snakemake.input.outerbin, **nighres_args)
+
+    print('binarized files to levelsets complete')
+
+
+    ## get isovolume solution
+    isovolume = nighres.laminar.volumetric_layering(levelset_inner['result'], levelset_outer['result'], **nighres_args)
+
+    print('levelset to isovolume complete')
+
+
+    ## copy to output (volumetric_layering produces multiple files so we need to specify the right one)
+    copyfile(isovolume['depth'], snakemake.output.coords)
+
