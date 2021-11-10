ca76f3e2f46a7ad2ad27aea46b7ed1a493449437
jordandekraker <jordandekraker@gmail.com>
2021-11-10 17:35:36 -0500

    uses morphometry+fastmarching to infer LaplacePD within the DG. not yet tested

---
+++ hippunfold/workflow/scripts/inferLaplacePD_DG.py
@@ -0,0 +1,128 @@
+import numpy as np
+import nibabel as nib
+from astropy.convolution import convolve as nan_convolve
+from scipy.interpolate import NearestNDInterpolator
+import skfmm
+#logfile = open(snakemake.log[0], 'w')
+
+# This script will get approximate Laplace_PD within the DG by fast marching edge-to-edge within small AP slices. Start in the middle of the hippocmapus and then NN copy to next slice and rerun fast marching both directions (to ensure the march goes in the same direction between slices!). Note that this requires endpoints to be most distant from eachother in each slice, as in a 'U' shape, but this isn't always the case if there is more of a 'V' shape!
+
+# test inputs
+lbl_nib = nib.load('test_T1w_dentate-noCA4/work/work/sub-01/seg_T1w/sub-01_hemi-R_space-corobl_desc-postproc_dseg.nii.gz')
+lbl = lbl_nib.get_fdata()
+gmlbl = [8]
+AP_nib = nib.load('test_T1w_dentate-noCA4/work/work/sub-01/seg_T1w/sub-01_dir-AP_hemi-R_space-corobl_desc-laplace_coords.nii.gz')
+AP = AP_nib.get_fdata()
+
+# real inputs
+#lbl_nib = nib.load(snakemake.input.lbl)
+#lbl = lbl_nib.get_fdata()
+#gmlbl = snakemake.params.lblgm
+#AP_nib = nib.load(snakemake.input.APcoords)
+#AP = AP_nib.get_fdata()
+#APsrc = snakemake.params.APsrc_labels
+#APsnk = snakemake.params.APsink_labels
+
+# params
+nslices = 30
+smooth_iters = 5
+ap = np.linspace(0,1,nslices)
+speed = np.zeros_like(lbl) + 0.01 # allow slow travel outside DG
+speed[lbl==8] = 1
+PD = np.zeros_like(AP)
+PD[:] = np.nan
+
+# start with middle AP slice
+i = nslices//2
+sl = np.logical_and(AP>ap[i-1], AP<ap[i])
+APslice_mid = np.logical_and(sl,lbl==8)
+[x,y,z] = np.where(APslice_mid)
+med = np.median(x) # sagittally the middle
+# march outward (both laterally and medially)
+v = np.where(x==med)[0]
+phi = np.ones_like(lbl)
+phi[x[v], y[v], z[v]] = 0
+forward = skfmm.travel_time(phi,speed)
+out = np.zeros_like(forward)
+out[APslice_mid] = forward[APslice_mid]
+v = np.where(x<med)[0] # block out lateral side so we always start medial
+out[x[v], y[v], z[v]] = 0
+# now march lateral
+[x,y,z] = np.where(out==np.max(out))
+phi = np.ones_like(lbl)
+phi[x[0],y[0],z[0]] = 0 # only the first point
+forward = skfmm.travel_time(phi,speed)
+PD[APslice_mid] = forward[APslice_mid]/np.max(forward[APslice_mid])
+print('fastmarch for middle AP slice done')
+
+# move to new slices (towards anterior)
+APslice_prev = APslice_mid
+for ii in range(i-1,0,-1):
+    sl = np.logical_and(AP>ap[ii-1], AP<ap[ii])
+    APslice = np.logical_and(sl,lbl==8)
+    if np.sum(APslice) > 0:
+        # interpolate this slice
+        [x,y,z] = np.where(APslice_prev)
+        interp = NearestNDInterpolator(np.c_[x,y,z],PD[APslice_prev])
+        [x,y,z] = np.where(APslice)
+        out = interp(x,y,z)
+        # march backward
+        v = np.where(out==(np.max(out)))[0] # most lateral
+        phi = np.ones_like(lbl)
+        phi[x[v], y[v], z[v]] = 0
+        forward = skfmm.travel_time(phi,speed)
+        out = forward[APslice]
+        # march forward
+        v = np.where(out==(np.max(out)))[0] # most lateral
+        phi = np.ones_like(lbl)
+        phi[x[v], y[v], z[v]] = 0
+        forward = skfmm.travel_time(phi,speed)
+        PD[APslice] = forward[APslice]/np.max(forward[APslice])
+        print('fastmarch for AP slice ' + str(ii) + ' done')
+    else:
+        print('skipping AP slice ' + str(ii) + ' not enough voxels')
+    APslice_prev = APslice
+
+# move to new slices (towards posterior)
+APslice_prev = APslice_mid
+for ii in range(i+1,nslices,1):
+    sl = np.logical_and(AP>ap[ii-1], AP<ap[ii])
+    APslice = np.logical_and(sl,lbl==8)
+    if np.sum(APslice) > 0:
+        # interpolate this slice
+        [x,y,z] = np.where(APslice_prev)
+        interp = NearestNDInterpolator(np.c_[x,y,z],PD[APslice_prev])
+        [x,y,z] = np.where(APslice)
+        out = interp(x,y,z)
+        # march backward
+        v = np.where(out==(np.max(out)))[0] # most lateral
+        phi = np.ones_like(lbl)
+        phi[x[v], y[v], z[v]] = 0
+        forward = skfmm.travel_time(phi,speed)
+        out = forward[APslice]
+        # march forward
+        v = np.where(out==(np.max(out)))[0] # most lateral
+        phi = np.ones_like(lbl)
+        phi[x[v], y[v], z[v]] = 0
+        forward = skfmm.travel_time(phi,speed)
+        PD[APslice] = forward[APslice]/np.max(forward[APslice])
+        print('fastmarch for AP slice ' + str(ii) + ' done')
+    else:
+        print('skipping AP slice ' + str(ii) + ' not enough voxels')
+    APslice_prev = APslice
+
+# smooth to clean up space between slices
+hl=np.zeros([3,3,3])
+hl[1,:,:] = 1
+hl[:,1,:] = 1
+hl[:,:,1] = 1
+hl[1,1,1] = 0
+hl = hl/np.sum(hl)
+PD_smooth = PD
+for n in range(smooth_iters):
+    PD_smooth = nan_convolve(PD_smooth,hl,preserve_nan=True)
+
+sv = nib.Nifti1Image(PD_smooth,AP_nib.affine,AP_nib.header)
+nib.save(sv,'test')
+
+
