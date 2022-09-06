8871cf47738aea94d9ce3a380f8739f22f40fe5f
Ali Khan <alik@robarts.ca>
2022-09-06 16:51:12 -0400

    refactored dag generation, now creates subgraph visualizations

--- hippunfold/dags/00_generate_all.sh
+++ hippunfold/dags/00_generate_all.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-hippunfold ../../test_data/bids_T1w /tmp/out participant -np --modality T1w --rulegraph --keep-work | ./generate_dag_images.sh T1w_rulegraph
-hippunfold ../../test_data/bids_singleT2w /tmp/out participant -np --modality T2w --t1-reg-template --rulegraph --keep-work | ./generate_dag_images.sh T2w_t1-reg-template_rulegraph
-hippunfold ../../test_data/bids_singleT2w /tmp/out participant -np --modality T2w --rulegraph --keep-work | ./generate_dag_images.sh T2w_rulegraph
-hippunfold ../../test_data/bids_hippb500 /tmp/out participant -np --modality hippb500 --rulegraph --keep-work | ./generate_dag_images.sh hippb500_rulegraph
+
+for graphtype in dag rulegraph
+do
+
+    hippunfold ../../test_data/bids_T1w /tmp/out participant -np --modality T1w --${graphtype} --keep-work | ./proc_subgraph.py out_${graphtype}/T1w
+    hippunfold ../../test_data/bids_T1w /tmp/out participant -np --modality T1w --${graphtype} --keep-work --hemi R -n | ./proc_subgraph.py out_${graphtype}/T1w_hemi-R
+    hippunfold ../../test_data/bids_singleT2w /tmp/out participant -np --modality T2w --${graphtype} --keep-work | ./proc_subgraph.py out_${graphtype}/T2w
+    hippunfold ../../test_data/bids_singleT2w /tmp/out participant -np --modality T2w --${graphtype} --t1-reg-template --keep-work | ./proc_subgraph.py out_${graphtype}/T2w_t1-reg-template
+    hippunfold ../../test_data/bids_hippb500 /tmp/out participant -np --modality hippb500 --${graphtype} --keep-work | ./proc_subgraph.py out_${graphtype}/hippb500
+
+done
