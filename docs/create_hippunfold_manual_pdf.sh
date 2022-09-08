94157e7050134e7d3b66d6f815423d9a2ee03c07
Ali Khan <alik@robarts.ca>
2022-09-08 12:30:11 -0400

    add script to generate latex manual

---
+++ docs/create_hippunfold_manual_pdf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euox pipefail
+
+git restore ../README.md
+tmpfile=`mktemp`
+cp ../README.md $tmpfile
+tail -n +5 $tmpfile > ../README.md
+make latexpdf
+cp _build/latex/hippunfold.pdf hippunfold_manual.pdf
+git restore ../README.md
+git add hippunfold_manual.pdf
+git commit -m "updated hippunfold manual"
+
