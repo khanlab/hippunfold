82e0f9230868c54e4718992c0d865069479aa9c3
Jordan DeKraker - B. Bernhardt Lab <jordand@bic.mni.mcgill.ca>
2023-02-10 14:31:22 -0500

    bug fixes and working for different surf densities

--- hippunfold/workflow/scripts/warp_flatsurf.py
+++ hippunfold/workflow/scripts/warp_flatsurf.py
@@ -1,14 +1,22 @@
 import numpy as np
 import nibabel as nib
 
-warp = nib.load(snakemake.input.invwarp).get_fdata()
+warp = nib.load(snakemake.input.invwarp[0])
+field = warp.get_fdata()
 surf = nib.load(snakemake.input.gii)
 vertices = surf.get_arrays_from_intent('NIFTI_INTENT_POINTSET')[0].data
 
+# put in same space
+#v = np.concatenate((vertices,np.ones((len(vertices),1))),1)
+#v = np.matmul(v,warp.affine)
+v = vertices / warp.affine[0,0]
+v = v - np.min(v,axis=0)
+v = v.astype(int)
+
 # apply warp (2D)
-warp = np.transpose(warp, (1,0,2,3,4))
-vertices[:,0] = vertices[:,0] + warp[:,:,:,:,0].flatten()
-vertices[:,1] = vertices[:,1] + warp[:,:,:,:,1].flatten()
+for i in range(len(vertices)):
+    vertices[i,0] = vertices[i,0] + field[v[i,0],v[i,1],0,0,0]
+    vertices[i,1] = vertices[i,1] + field[v[i,0],v[i,1],0,0,1]
 
 surf.get_arrays_from_intent('NIFTI_INTENT_POINTSET')[0].data = vertices
-nib.save(surf,snakemake.output.gii')
+nib.save(surf,snakemake.output.gii)
