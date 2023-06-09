8e97f74913fac60ed248e8111fa0570ae0f311a4
Jordan DeKraker <jdekrake@uwo.ca>
2023-06-09 11:47:13 -0400

    Warps fix (#240)
    * small but complicated fix, will comment in PR thread
    
    * even lighter changes - dir-IO only
    
    * linting
    
    * eplsilon defined in config
    
    * including epsilon for dentate
    
    * formatting str to float
    
    ---------
    
    Co-authored-by: Jordan DeKraker - B. Bernhardt Lab <jordand@bic.mni.mcgill.ca>

--- hippunfold/workflow/scripts/create_warps.py
+++ hippunfold/workflow/scripts/create_warps.py
@@ -118,10 +118,11 @@ points = (
 
 # get unfolded grid (from 0 to 1, not world coords), using meshgrid:
 #  note: indexing='ij' to swap the ordering of x and y
+epsilon = snakemake.params.epsilon
 (unfold_gx, unfold_gy, unfold_gz) = np.meshgrid(
-    np.linspace(0, 1, unfold_dims[0]),
-    np.linspace(0, 1, unfold_dims[1]),
-    np.linspace(0, 1, unfold_dims[2]),
+    np.linspace(0 + float(epsilon[0]), 1 - float(epsilon[0]), unfold_dims[0]),
+    np.linspace(0 + float(epsilon[1]), 1 - float(epsilon[1]), unfold_dims[1]),
+    np.linspace(0 + float(epsilon[2]), 1 - float(epsilon[2]), unfold_dims[2]),
     indexing="ij",
 )
 
