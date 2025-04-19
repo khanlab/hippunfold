440c2a761de36c3fbac6b21ab4351ce2ce366d1d
Ali Khan <alik@robarts.ca>
2025-04-18 20:54:07 -0400

    use pybids parsing for dsegtissue, by using --derivatives
    - must have --derivatives point to the bids dir
    - also needs desc-tissue or desc-subfields
    - multihist7 datalad dataset is now structured this way to make atlas
      building easier

--- .dryrun_test_all.sh
+++ .dryrun_test_all.sh
@@ -9,10 +9,10 @@ set -euxo pipefail
 ./hippunfold/run.py test_data/bids_hippb500 test_out participant -np --modality hippb500 
 ./hippunfold/run.py test_data/bids_T1w_longitudinal test_out participant -np --modality T1w 
 ./hippunfold/run.py test_data/bids_singleT2w_longitudinal test_out participant -np --modality T2w 
-./hippunfold/run.py test_data/bids_dsegtissue test_out participant -np --modality dsegtissue --path_dsegtissue test_data/bids_dsegtissue/sub-{subject}/anat/sub-{subject}_hemi-{hemi}_desc-tissue_dseg.nii.gz
-./hippunfold/run.py test_data/bids_dsegtissue_1hemi test_out participant -np --modality dsegtissue --hemi L
+./hippunfold/run.py test_data/bids_dsegtissue test_out participant -np --modality dsegtissue --derivatives test_data/bids_dsegtissue 
+./hippunfold/run.py test_data/bids_dsegtissue_1hemi test_out participant -np --modality dsegtissue --derivatives test_data/bids_dsegtissue_1hemi --hemi L
 ./hippunfold/run.py test_data/bids_singleT2w test_out participant -np --modality T2w --t1_reg_template 
 ./hippunfold/run.py test_data/bids_singleT2w test_out participant -np --modality T2w --output_space T1w 
 ./hippunfold/run.py test_data/bids_T1w test_out participant -np --modality T1w --use-template-seg 
 ./hippunfold/run.py test_data/bids_singleT2w test_out participant -np --modality T2w --generate-myelin-map
-./hippunfold/run.py test_data/bids_dsegtissue test_out group_create_atlas -np --modality dsegtissue --path_dsegtissue test_data/bids_dsegtissue/sub-{subject}/anat/sub-{subject}_hemi-{hemi}_desc-tissue_dseg.nii.gz --path-dsegsubfields test_data/bids_dsegtissue/sub-{subject}/anat/sub-{subject}_hemi-{hemi}_desc-subfields_dseg.nii.gz --new-atlas-name mytestatlas
+./hippunfold/run.py test_data/bids_dsegtissue test_out group_create_atlas -np --modality dsegtissue --new-atlas-name mytestatlas
