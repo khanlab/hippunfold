e681fe0bd697df66336264d52d66ffaa0e5492e4
Dhananjhay <dhananjhay03@gmail.com>
2025-04-26 15:51:16 -0400

    change executable's path from relative to absolute path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 DEFAULT_FLAGS="--use-conda --conda-prefix /src/conda-envs"
 
 # Run the command, passing the default flags and any additional arguments
-exec conda run --no-capture-output -n snakebids-env ./hippunfold/run.py "$DEFAULT_FLAGS" "$@"
+exec conda run --no-capture-output -n snakebids-env /src/hippunfold/run.py "$DEFAULT_FLAGS" "$@"
